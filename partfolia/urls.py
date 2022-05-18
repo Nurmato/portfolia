4a10afa77d85bf3f95de643238d6fb55b7d02b05
Nurmato <umidn0246@gmail.com>
2022-05-18 15:19:31 +0200

    added static files,css and images

--- partfolia/urls.py
+++ partfolia/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('homepage',homepage,name='homepage'),
+    path('',homepage,name='homepage'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
