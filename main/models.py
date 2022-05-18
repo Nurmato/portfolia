45094ec63b84b5ea4534faa982131706595be19d
Nurmato <umidn0246@gmail.com>
2022-05-18 17:17:09 +0200

    created model.connected DB Postgres.added button,from,name in index.html,

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    email = models.EmailField('Email почта', max_length=255)
+    number_of_telephone = models.CharField('Number Phone', max_length=255)
+    message = models.TextField('Message')
+    sent_at = models.DateTimeField(auto_now_add=True)
